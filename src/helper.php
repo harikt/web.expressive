cf2d6fd7bbb07edd89dcf42c2c1a6d088b93e76f
Hari KT <kthari85@gmail.com>
2017-10-10 10:42:05 +0530

    Remove function event

--- src/helper.php
+++ src/helper.php
@@ -213,21 +213,6 @@ if (! function_exists('elixir')) {
     }
 }
 
-if (! function_exists('event')) {
-    /**
-     * Dispatch an event and call the listeners.
-     *
-     * @param  string|object $event
-     * @param  mixed         $payload
-     * @param  bool          $halt
-     * @return array|null
-     */
-    function event(...$args)
-    {
-        return app('events')->dispatch(...$args);
-    }
-}
-
 if (! function_exists('public_path')) {
     /**
      * Get the path to the public folder.
